66c10b39ce0bbffd91d9acbcc9a355a98deba191
azzraaa <azrkor26@gmail.com>
2026-02-27 21:36:35 +0100

    Added jump function

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var speed: float = 300.0
+@export var jump_height: float = 20
+
+var vertical_velocity: float = 0.0
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
@@ -17,3 +20,13 @@ func _physics_process(delta):
 	# Move the player
 	velocity = direction.normalized() * speed
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		jump()
+
+
+func jump():
+	print("Player jumps!")
+	position.y -= jump_height   # move visually up
+	await get_tree().create_timer(0.1).timeout   # wait 0.1 seconds
+	position.y += jump_height   # move back down
